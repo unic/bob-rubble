b7b021c7af35281ad7be03c9c49528aa98628639
Yannis Guedel <yannis.guedel@unic.com>
2015-02-04 13:04:41 +0100

    fixed problem with resolvepath

--- src/tools/Rubble/Rubble.psm1
+++ src/tools/Rubble/Rubble.psm1
@@ -1,21 +1,20 @@
 $PSScriptRoot = Split-Path  $script:MyInvocation.MyCommand.Path
 
-function ResolvePath() {
-  param($PackageId, $RelativePath)
-  $paths = @("$PSScriptRoot\..\..\..\packages", "$PSScriptRoot\..\packages")
-  foreach($packPath in $paths) {
-    $path = Join-Path $packPath "$PackageId\$RelativePath"
-    if((Test-Path $packPath) -and (Test-Path $path)) {
-      Resolve-Path $path
-      return
-    }
-  }
-  Write-Error "No path found for $RelativePath in package $PackageId"
-}
+Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1 | Foreach-Object{ . $_.FullName }
+Export-ModuleMember -Function * -Alias *
 
+function ResolvePath() {
+    param($PackageId, $RelativePath)
+    $paths = @("$PSScriptRoot\..\..\..\packages", "$PSScriptRoot\..\packages")
+    foreach($packPath in $paths) {
+        $path = Join-Path $packPath "$PackageId\$RelativePath"
+        if((Test-Path $packPath) -and (Test-Path $path)) {
+            Resolve-Path $path
+            return
+        }
+    }
+    Write-Error "No path found for $RelativePath in package $PackageId"
+}
 
 $zipDll = ResolvePath -PackageId "SharpZipLib" -RelativePath "lib\20\ICSharpCode.SharpZipLib.dll"
 [System.Reflection.Assembly]::LoadFrom($zipDll.Path) | Out-Null
-
-Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1 | Foreach-Object{ . $_.FullName }
-Export-ModuleMember -Function * -Alias *
