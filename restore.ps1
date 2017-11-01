fdc8395a016abca5c182c79e5a79f2dc96303a0f
test <test@test.net>
2017-11-01 22:12:48 +0100

    Update Paket Boostrapper and fix cmdline call.

--- restore.ps1
+++ restore.ps1
@@ -2,9 +2,9 @@ $Invocation = (Get-Variable MyInvocation -Scope 0).Value
 $PSScriptRoot = Split-Path $Invocation.MyCommand.Path
 
 $paketFolder = "$PSScriptRoot\.paket"
-$paketBoot = "$paketFolder\paket.bootstrapper.exe --prefer-nuget"
+$paketBoot = "$paketFolder\paket.bootstrapper.exe"
 
-& $paketBoot
+& $paketBoot --prefer-nuget
 & "$paketFolder\paket.exe" restore
 
 if($LASTEXITCODE -ne 0) {
