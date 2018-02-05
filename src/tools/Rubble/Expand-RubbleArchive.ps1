d8628c8f1cbbb001401627632e2afaec35e63c22
David Szöke <david.szoeke@unic.com>
2018-02-05 17:08:39 +0100

    Add FileFilter param to Expand-RubbleArchive

--- src/tools/Rubble/Expand-RubbleArchive.ps1
+++ src/tools/Rubble/Expand-RubbleArchive.ps1
@@ -11,6 +11,9 @@ The path to the ZIP file.
 .PARAMETER OutputLocation
 The path where to extract the content of the file.
 
+.PARAMETER FileFilter
+If the file filter is not a null string, it is interpreted as a list of one or more regular expressions which is tested against each file name.
+
 .EXAMPLE
 Expand-RubbleArchive -Path D:\temp\archive.zip -OutputLocation d:\temp\output
 
@@ -22,11 +25,12 @@ function Expand-RubbleArchive
         [Parameter(Mandatory=$true)]
         [string] $Path,
         [Parameter(Mandatory=$true)]
-        [string] $OutputLocation
+        [string] $OutputLocation,
+        [string] $FileFilter = $null
     )
     Process
     {
       $zip = New-Object ICSharpCode.SharpZipLib.Zip.FastZip
-      $zip.ExtractZip($Path,$OutputLocation, $null)
+      $zip.ExtractZip($Path,$OutputLocation, $FileFilter)
     }
 }
